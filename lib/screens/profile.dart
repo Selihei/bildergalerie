8c28e2bb75b5218b42f91398d0585f40f49c6592
Selihei <selina.heinrichlp@gmail.com>
2025-07-24 12:25:52 +0200

    added Scrollview Profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,13 +1,16 @@
-
 import  'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
+Future<String> loadProfileText() async {
+    return await rootBundle.loadString('assets/text/profile_text.txt');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column( 
+    return Column( 
         children: [
           SizedBox(height: 20,),
           CircleAvatar(
@@ -18,9 +21,28 @@ class ProfileScreen extends StatelessWidget {
           Text("Kaya Müller", style: TextStyle(fontWeight: FontWeight.w500, fontSize: 25)),
           Text("Fotograf", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
           SizedBox(height: 30),
-          Text("Hier kommt noch was", textAlign: TextAlign.justify, style: TextStyle(fontSize: 16),)
+           Expanded(
+              child: FutureBuilder<String>(
+                future: loadProfileText(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: SingleChildScrollView(
+                        child: Text(
+                          snapshot.data!,
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    );
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
+              },
+            ),
+          ),
         ],
-      ),
-    );
+      );
   }
 }
